cf3175f5018ede91135b1f3f0b7354dd996759f3
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-08 11:20:35 +0530

    test: gulp-gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const prettier = require('gulp-prettier');
+const deploy = require('gulp-gh-pages');
 
 gulp.task('default', () => {
   gulp.watch('jasmine/spec/feedreader.js').on('change', browserSync.reload);
@@ -13,4 +14,9 @@ gulp.task('default', () => {
     .src('jasmine/spec/feedreader.js')
     .pipe(prettier({ singleQuote: true }))
     .pipe(gulp.dest('jasmine/spec/'));
+});
+
+gulp.task('deploy', () => {
+  return gulp.src('./')
+  .pipe(deploy());
 });
