c6ff844ef9ddc807bec28ce971a2392db078911a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-08 08:49:45 +0530

    Delete package-lock.json file

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -72,22 +72,24 @@ $(
         document.querySelector('a.menu-icon-link').click();
         expect(document.body.classList.contains('menu-hidden')).toBe(true);
       });
+    });
 
-      /* TODO: Write a new test suite named "Initial Entries" */
-      describe('Initial Entries', () => {
-        /* TODO: Write a test that ensures when the loadFeed
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', () => {
+      /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        beforeEach(done => {
-          loadFeed(0, done);
-        });
+      beforeEach(done => {
+        loadFeed(0, done);
+      });
 
-        it('are present', () => {
-          expect($('.feed .entry').length).toBeGreaterThan(0);
-        });
+      it('are present', (done) => {
+        let numEntries = document.querySelector('.feed').getElementsByClassName('entry').length;
+        expect(numEntries).toBeGreaterThan(0);
+        done();
       });
     });
 
