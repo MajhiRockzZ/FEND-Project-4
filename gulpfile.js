77130a0f7295042731ec1c65bfcb3db95512b350
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-08 15:59:57 +0530

    Setting up workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,48 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const prettier = require('gulp-prettier');
-const deploy = require('gulp-gh-pages');
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const cssnano = require('gulp-cssnano');
+const del = require('del');
+const runSequence = require('run-sequence');
 
-gulp.task('default', () => {
-  gulp.watch('jasmine/spec/feedreader.js').on('change', browserSync.reload);
-
-  browserSync.init({
-    server: './'
-  });
-
-  return gulp
-    .src('jasmine/spec/feedreader.js')
-    .pipe(prettier({ singleQuote: true }))
-    .pipe(gulp.dest('jasmine/spec/'));
+gulp.task('default', callback => {
+  runSequence(['useref', 'fonts', 'watch'], callback);
 });
 
-gulp.task('deploy', () => {
-  return gulp.src('./')
-  .pipe(deploy());
+gulp.task('watch', ['browserySync'], () => {
+  gulp.watch('app/css/**/*.css', browserSync.reload);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+  gulp.watch('app/jasmine/**/*.js', browserSync.reload);
+});
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+
+gulp.task('useref', () => {
+  return gulp
+    .src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('clean:dist', () => {
+  return del.sync('dist');
+});
+
+gulp.task('build', callback => {
+  runSequence('clean:dist', ['useref', 'fonts'], callback);
 });
